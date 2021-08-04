bfd74fedfe1e48144095e9a83b2d704c1dfb44ce
sorokindmitrij <tipalol2@gmail.com>
2021-08-04 16:17:41 +0500

    Created project and added logging

---
+++ EmployeeAssistant.IO.Telegram/Controllers/TelegramController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeAssistant.IO.Telegram.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TelegramController : ControllerBase
+    {
+        
+    }
+}
